131bff7b72265f491eca81d70eadcf07a8db01db
Yuhao LI <yuhao.li@student.unimelb.edu.au>
2023-02-08 15:44:03 +1100

    Add multivariate lasso code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Bayesian-Lasso
-My honour year project
+# Local-Global-Algorithm for Bayesian Lasso Problem
+**Author: Yuhao Li**
+
+Developing an fast and efficient variational approximation correction algorithm to improve posterior parameter distribution obtained by mean field variational bayes for fast and accurate approximate bayesian inference. We've also applied it to several real dataset and achieve excellent approximation result.
+
+## Introduction
+
+Lasso penalized regression is a popular technique for simultaneous coefficient estimation and variable selection. Issues associated with this method include potential sensitivity of estimation and variable selection to the choice of tuning parameter, and calculating appropriate standard errors of the estimates. This project will adopt a Bayesian approach and develop various fast Approximate Bayesian Inference (ABI) methods for this problem. ABI methods include Gaussian Variational Approximate inference, Moment Propagation, Approximate Posterior Statistic Matching, and Population Based Variational Bayes (time permitting). A variety of model representations will also be considered. We compare these to much slower MCMC and frequentist methods to assess the accuracy of standard error estimates for ABI methods, as well as model selection accuracy.
---
+++ real_dataset_code/code/lasso_distribution/Multi-Lasso.R
@@ -0,0 +1,74 @@
+###################  MULTIVARIATE LASSO DISTRIBUTION ###############################
+library(MASS)
+library(mvtnorm)
+# Note: A: positive definite,c vb in R2, cc>0, 
+# Note: a>0, c>0
+dmlasso <- function(x,a,b,c,logarithm=FALSE) 
+{
+  log_Z <- alasso(a,b,c) 
+  log_pdf <-  -0.5*t(x)%*%A%*%x + b %*% x - c*norm(x,"1") - log_Z
+  
+  if (logarithm) {
+    return(log_pdf) 
+  }  
+  return(exp(log_pdf))
+}
+
+zmlasso <- function(A,b,c)
+{
+  # Get parameter
+  A_inv = solve(A)
+  mu1 = as.vector(A_inv %*%(b - c))
+  mu2 = as.vector(A_inv %*% matrix(c(-b[1]-c,b[2]-c),2,1))
+  mu3 = as.vector(A_inv %*% matrix(c(b[1]-c,-b[2]-c),2,1))
+  mu4 = as.vector(A_inv %*%(b + c))
+  Sigma = A_inv
+  
+  # Get 4 parts of equation separately
+  PartI = pmvnorm(0,Inf,mean = mu1, sigma = Sigma)/dmvnorm(as.vector(A%*%mu1),sigma = A)
+  PartII = pmvnorm(0,Inf,mean = mu2, sigma =Sigma)/dmvnorm(as.vector(A%*%mu2),sigma = A)
+  PartIII = pmvnorm(0,Inf,mean = mu3, sigma = Sigma)/dmvnorm(as.vector(A%*%mu3),sigma = A)
+  PartIV = pmvnorm(0,Inf,mean = mu4, sigma=Sigma)/dmvnorm(as.vector(A%*%mu4),sigma = A)
+  
+  
+  return((PartI+PartII+PartIII+PartIV)[1]*det(A))
+}
+
+# Calculate expectation of lasso distribution
+emlasso<- function(A,b,c)
+{
+  mean = (PartI*multi_pos_tru_mean(mu1,sigma) + PartII*pos_tru_xxt(mu1,sigma) + PartIII + PartIV)/Z
+  
+  return(mean)
+}
+
+# Calculate convariance matrix of multivariate lasso distribution
+vmlasso = function(PartI, PartII, PartIII, PartIV, Z)
+{
+  Sigma =  (PartI*multi_pos_tru_mean(mu1,sigma) + PartII*pos_tru_xxt(mu1,sigma) + PartIII + PartIV)/Z
+  return(Sigma)
+}
+
+# A = matrix(c(2,0,0,3),2,2)
+# b = matrix(c(-2,1),2,1)
+# c = 3
+# calculate_multi_lasso_param(A,b,c)
+
+
+
+# library(pracma)
+# fun <- function(x,y) 
+# {
+#   A = matrix(c(2,0,0,3),2,2)
+#   b = matrix(c(-2,1),2,1)
+#   c = 3
+#   vx = matrix(c(x,y),2,1)
+#   return(exp((-0.5*t(vx)%*%A%*%vx + t(b)%*%vx-  c*norm(vx,"1"))[1]))
+# }
+# xmin <- -10; xmax <- 10
+# ymin <- -10; ymax <- 10
+# 
+# # Normalizing constant
+# integral2(fun, xmin, xmax, ymin, ymax)
+
+
