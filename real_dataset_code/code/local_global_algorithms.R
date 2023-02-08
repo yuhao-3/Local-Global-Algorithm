b6f550ce6380a13eb6b1c5511a07a0888a95c7fe
Yuhao LI <yuhao.li@student.unimelb.edu.au>
2023-02-08 18:57:12 +1100

    Clean the code

---
+++ real_dataset_code/code/local_global_algorithms.R
@@ -0,0 +1,93 @@
+source(here("code","bayesian_lasso_gibbs.R")) 
+## INPUT: y, mX, lambda, param
+## OUTPUT: Global adjust mean, Global adjust covariance
+
+## If there is a scalar output, change it to 1*1 matrix output, otherwise remain same
+
+
+local_global_algorithm_1 <- function(vy, mX, lambda, params)
+{
+  ## Initialization
+  MAXITER <- 500
+  TOL <- 1.0E-8
+  n <- nrow(mX)
+  p <- ncol(mX)
+  XTX = t(mX)%*%mX
+  rho = 0.8
+  
+  ## Initial Global Mean and Global Covariance From MFVB
+  vmu_adjust = params$vmu_til
+  mSigma_adjust = params$mSigma_til
+  
+  # Later will be changed
+  a_til = params$a_til
+  b_til = params$b_til
+  c = lambda * gamma(a_til+0.5)/(gamma(a_til)*sqrt(b_til))
+  
+  # Record the local parameter
+  va = c()
+  vb = c()
+  vZ = c()
+  
+  # Initial value Local parameters
+  vtheta <- c(vmu_adjust)
+  
+  for (ITER in 1:MAXITER)
+  {
+    ## Local Update
+    for (j in 1:p)
+    {
+      # Store parameter for previous iteration
+      vmu_old = vmu_adjust
+      mSigma_old = mSigma_adjust
+      
+      # Define some constant
+      j_len = length(vmu_adjust[j])
+      mSigma_jj_inv = matrix(solve(mSigma_adjust[j,j]),j_len,j_len)
+      mt = matrix(mSigma_adjust[-j,j]) %*% mSigma_jj_inv
+      vs = matrix(vmu_adjust[-j]) - mt %*% matrix(vmu_adjust[j])
+      
+      # Local Update
+      ## Update local parameter
+      a = a_til/b_til*(matrix(XTX[j,j]) + t(XTX[j,-j]%*%mt))
+      b = a_til/b_til*t(mX[,j])%*%(vy-mX[,-j]%*%vs)
+      
+      ## Calculate Local mean and variance
+      vlocal_mean = elasso(a,b,c)
+      mlocal_var = vlasso(a,b,c)
+      
+      # Record local parameter
+      va[j] = a
+      vb[j] = b
+      vZ[j] = zlasso(a,b,c)
+      
+      # Global Update
+      ## Update Mean
+      vmu_adjust[j] = vlocal_mean
+      vmu_adjust[-j] = matrix(vmu_adjust[-j]) + matrix(mSigma_adjust[-j,j]) %*%  mSigma_jj_inv  %*% matrix(vlocal_mean - vmu_old[j])
+      
+      ## Update Covariance
+      mSigma_adjust[j,j] = mlocal_var
+      mSigma_adjust[j,-j] = mlocal_var %*% mSigma_jj_inv %*%  t(matrix(mSigma_old[j,-j]))
+      mSigma_adjust[-j,j] = t(mSigma_adjust[j,-j])
+      mSigma_adjust[-j,-j] = matrix(mSigma_old[-j,-j],p-j_len,p-j_len) + matrix(mSigma_old[-j,j]) %*% mSigma_jj_inv %*% (mlocal_var - matrix(mSigma_old[j,j]))  %*%mSigma_jj_inv %*% t(matrix(mSigma_old[j,-j]))
+      
+      # Damping
+      # mSigma_adjust = rho* mSigma_adjust + (1-rho)*mSigma_old
+    }
+    
+    # Update Theta
+    vtheta_old <- vtheta
+    vtheta <- c(vmu_adjust)
+    
+    ## Check stopping criterion    
+    err <- max(abs(vtheta - vtheta_old))
+    cat("ITER=",ITER,"err=",err,"\n")
+    if (err < TOL) {
+      break;
+    }
+  }
+  # Return Local and Global parameter
+  return(list("vmu_til" = vmu_adjust,"mSigma_til" = mSigma_adjust, "a" = va,"b" = vb,"c"= c ,"Z" = vZ))
+  
+}
